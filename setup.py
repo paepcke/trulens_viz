b401cd522dcb2e3f45c94baf2102101715b749f9
Andreas Paepcke <paepcke@cs.stanford.edu>
2022-04-11 12:02:24 -0700

    Added stump setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+    name = "trulens_viz",
+    version = "0.01",
+    author = "Andreas Paepcke",
+    author_email = "paepcke@cs.stanford.edu",
+    description = "Draft of NLP related attribution strength",
+    url = "https://github.com/paepcke/trulens_viz",
+    )
